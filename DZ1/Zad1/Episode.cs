4be82ea53eb57e678b78f25e189267c5a98cb1b7
BartolOce <lovro.bartolovic15@gmail.com>
2022-10-29 18:39:09 +0200

    Rjesenje zadace 1

---
+++ DZ1/Zad1/Episode.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    public class Episode
+    {
+        int viewers;
+        double sum;
+        double max;
+
+        public Episode()
+        {
+            viewers = 0;
+            sum = 0;
+            max = 0;
+        }
+
+        public Episode(int viewers, double sum, double max)
+        {
+            this.viewers = viewers;
+            this.sum = sum;
+            this.max = max;
+        }
+
+        public void AddView(double score)
+        {
+            viewers++;
+
+            sum += score;
+
+            if (max < score)
+            {
+                max = score;
+            }
+        }
+
+        public double GetMaxScore()
+        {
+            return max;
+        }
+
+        public double GetAverageScore()
+        {
+            return sum / viewers;
+        }
+
+        public int GetViewerCount()
+        {
+            return viewers;
+        }
+    }
+}
