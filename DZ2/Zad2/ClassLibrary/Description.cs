82b2dff21eccb26d6d902cc3e0bf80ee0cbb4c6e
BartolOce <lovro.bartolovic15@gmail.com>
2022-11-18 14:21:55 +0100

    Zadaca 2

---
+++ DZ2/Zad2/ClassLibrary/Description.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Description
+    {
+        public int Number { get; set; }
+        public TimeSpan Lenght { get; set; }
+        public string Name { get; set; }
+
+        public Description(int episodeNumber, TimeSpan episodeLenght, string episodeName)
+        {
+            this.Number = episodeNumber;
+            this.Lenght = episodeLenght;
+            this.Name = episodeName;
+        }
+
+        public override string ToString()
+        {
+            return Number + "," + Lenght + "," + Name;
+        }
+    }
+}
---
+++ DZ2/Zad2/ClassLibrary/Episode.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Episode
+    {
+        public int Viewers { get; set; }
+        public double Sum { get; set; }
+        public double Max { get; set; }
+        public Description Description { get; set; }
+
+        public Episode(int viewers, double sum, double max, Description description)
+        {
+            Viewers = viewers;
+            Sum = sum;
+            Max = max;
+            Description = description;
+        }
+
+        public void AddView(double score)
+        {
+            Viewers++;
+
+            Sum += score;
+
+            if (Max < score)
+            {
+                Max = score;
+            }
+        }
+
+        public double GetMaxScore()
+        {
+            return Max;
+        }
+
+        public double GetAverageScore()
+        {
+            return Sum / Viewers;
+        }
+
+        public int GetViewerCount()
+        {
+            return Viewers;
+        }
+
+        public override string ToString()
+        {
+            string returnString = Viewers + "-" + Sum + "-" + Max + "-" + Description.Number + "-" + Description.Lenght + "-" + Description.Name;
+            return returnString.Replace(",",".").Replace("-",",");
+        }
+
+        public static bool operator <(Episode lhs, Episode rhs)
+        {
+            return lhs.GetAverageScore() < rhs.GetAverageScore();
+        }
+        public static bool operator >(Episode lhs, Episode rhs)
+        {
+            return lhs.GetAverageScore() > rhs.GetAverageScore();
+        }
+    }
+}
