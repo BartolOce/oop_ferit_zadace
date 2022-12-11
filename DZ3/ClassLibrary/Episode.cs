34a25e680ba228797a11af154f544409a1e6e2e5
BartolOce <lovro.bartolovic15@gmail.com>
2022-12-11 21:25:00 +0100

    Zadaca 3

---
+++ DZ3/ClassLibrary/Episode.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Episode
+    {
+        public int Viewers { get; set; }
+        public double Sum { get; set; }
+        public double Max { get; set; }
+        public Description Description { get; set; }
+
+        public Episode(int viewers, double sum, double max, Description description)
+        {
+            Viewers = viewers;
+            Sum = sum;
+            Max = max;
+            Description = description;
+        }
+
+        public void AddView(double score)
+        {
+            Viewers++;
+
+            Sum += score;
+            Sum = Math.Round(Sum, 4);
+
+            if (Max < score)
+            {
+                Max = score;
+                Max = Math.Round(Max, 2);
+            }
+        }
+
+        public double GetMaxScore()
+        {
+            return Max;
+        }
+
+        public double GetAverageScore()
+        {
+            return Sum / Viewers;
+        }
+
+        public int GetViewerCount()
+        {
+            return Viewers;
+        }
+
+        public override string ToString()
+        {
+            string returnString = Viewers + "-" + Sum + "-" + Max + "-" + Description.Number + "-" + Description.Lenght + "-" + Description.Name;
+            return returnString.Replace(",",".").Replace("-",",");
+        }
+
+        public static bool operator <(Episode lhs, Episode rhs)
+        {
+            return lhs.GetAverageScore() < rhs.GetAverageScore();
+        }
+        public static bool operator >(Episode lhs, Episode rhs)
+        {
+            return lhs.GetAverageScore() > rhs.GetAverageScore();
+        }
+    }
+}
